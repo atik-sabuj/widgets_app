8b8f4060bf37d32fb87b53dc1c3c7d3bd804547a
Sabuj <sabu235@gmail.com>
2023-11-29 15:52:55 +0600

    ReorderableListView widget #2

--- lib/widgets/reorderablelistview_widget.dart
+++ lib/widgets/reorderablelistview_widget.dart
@@ -8,8 +8,38 @@ class ReorderableWidget extends StatefulWidget {
 }
 
 class _ReorderableWidgetState extends State<ReorderableWidget> {
+
+  List<String> _productList = ['Apple', 'Mango', 'Bananna', 'Strabery', 'Cherry'];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FolMul.com'),
+      ),
+
+      body: ReorderableListView.builder(
+          itemBuilder: (context, index){
+            return Card(
+              key: ValueKey(_productList[index]),
+              child: ListTile(
+                title: Text(_productList[index]),
+              ),
+            );
+          },
+
+          itemCount: _productList.length,
+          onReorder: (oldIndex, newIndex){
+            setState(() {
+              if(newIndex > oldIndex) {
+                newIndex = newIndex - 1;
+              }
+              final element = _productList.removeAt(oldIndex);
+              _productList.insert(newIndex, element);
+            });
+
+          }
+      ),
+    );
   }
 }
