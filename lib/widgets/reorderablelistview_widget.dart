5ce814df40d30bb2d14ab4e8684ac05231e5c7fe
Sabuj <sabu235@gmail.com>
2023-11-29 15:34:12 +0600

    ReorderableListView widget

---
+++ lib/widgets/reorderablelistview_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReorderableWidget extends StatefulWidget {
+  const ReorderableWidget({super.key});
+
+  @override
+  State<ReorderableWidget> createState() => _ReorderableWidgetState();
+}
+
+class _ReorderableWidgetState extends State<ReorderableWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
