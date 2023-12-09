837b6ffee13c480c1b1124ffff0f9e4290a53e23
Sabuj <sabu235@gmail.com>
2023-12-09 11:41:51 +0600

    Country Code Picker Widget

---
+++ lib/widgets/country_code_picker_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CountryCodePickerWidget extends StatefulWidget {
+  const CountryCodePickerWidget({super.key});
+
+  @override
+  State<CountryCodePickerWidget> createState() => _CountryCodePickerWidgetState();
+}
+
+class _CountryCodePickerWidgetState extends State<CountryCodePickerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
