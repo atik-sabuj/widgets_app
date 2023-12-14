6ee142d754bd33911a8411e9ec0fa52384aab279
Sabuj <sabu235@gmail.com>
2023-12-14 09:27:18 +0600

    Animated Container Widget #2

--- lib/widgets/animated_container_widget.dart
+++ lib/widgets/animated_container_widget.dart
@@ -8,8 +8,29 @@ class AnimatedContainerWidget extends StatefulWidget {
 }
 
 class _AnimatedContainerWidgetState extends State<AnimatedContainerWidget> {
+
+  double height = 100;
+  double width = 100;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animated Container Widget'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          height: height,
+          width: width,
+          decoration: BoxDecoration(
+            color: Colors.red
+          ),
+          duration: Duration(seconds: 1),
+
+        ),
+      ),
+
+    );
   }
 }
