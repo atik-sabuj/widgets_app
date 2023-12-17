08a4c447685e8ebec1ddef15180579fc69a279d3
Sabuj <sabu235@gmail.com>
2023-12-17 17:47:49 +0600

    Menu Page

---
+++ lib/widgets/menu_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MenuPage extends StatelessWidget {
+  const MenuPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Menu Page'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.blue,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
