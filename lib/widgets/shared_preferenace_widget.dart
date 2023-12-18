51cf1e954397481efbc79fd4c2ff4d096ec6ad8a
atik-sabuj <sabu235@gmail.com>
2023-12-18 11:30:03 +0600

    Shared Preference Widget

---
+++ lib/widgets/shared_preferenace_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SharedPreferenceWidget extends StatefulWidget {
+  const SharedPreferenceWidget({super.key});
+
+  @override
+  State<SharedPreferenceWidget> createState() => _SharedPreferenceWidgetState();
+}
+
+class _SharedPreferenceWidgetState extends State<SharedPreferenceWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
