50a323f6b9c8041157923a0638865d02a0211baf
atik-sabuj <sabu235@gmail.com>
2023-12-20 16:32:12 +0600

    Custom Drop Down Widget

--- lib/widgets/custom_drop_down_widget.dart
+++ lib/widgets/custom_drop_down_widget.dart
@@ -8,21 +8,12 @@ class CustomDropDownWidget extends StatefulWidget {
 }
 
 class _CustomDropDownWidgetState extends State<CustomDropDownWidget> {
-
-  bool isExpanded = false ;
-  String selectedValue = 'Select option' ;
-
-  //list that will be expanded
-  List<String> politicsList = ['Left Wing', 'Liberal', 'Moderate', 'Conservative', 'Libertarian', 'Apolitical'] ;
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Drop Down'),
       ),
-
     );
   }
 
