99880c706d9bed04833a202012df8fc535c86407
atik-sabuj <sabu235@gmail.com>
2023-12-21 11:56:51 +0600

    Internet Connectivity Screen

---
+++ lib/widgets/internet_connectivity_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InternetConnectivityScreen extends StatefulWidget {
+  const InternetConnectivityScreen({super.key});
+
+  @override
+  State<InternetConnectivityScreen> createState() => _InternetConnectivityScreenState();
+}
+
+class _InternetConnectivityScreenState extends State<InternetConnectivityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
