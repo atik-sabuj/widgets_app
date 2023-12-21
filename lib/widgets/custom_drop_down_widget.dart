03fe65b0087653674b35f0c8608a0401787c4706
atik-sabuj <sabu235@gmail.com>
2023-12-21 11:32:21 +0600

    Custom Drop Down Widget #5

--- lib/widgets/custom_drop_down_widget.dart
+++ lib/widgets/custom_drop_down_widget.dart
@@ -9,9 +9,9 @@ class CustomDropDownWidget extends StatefulWidget {
 
 class _CustomDropDownWidgetState extends State<CustomDropDownWidget> {
 
-
+  bool isOpen = false;
   String selectOptions = 'Select Options';
-  List<String> politics = ['Left Wing', 'Liberal', 'Moderate', 'Conservative', 'Liberation', 'Apolitical'];
+  List<String> politicsList = ['Left Wing', 'Liberal', 'Moderate', 'Conservative', 'Liberation', 'Apolitical'];
 
   @override
   Widget build(BuildContext context) {
@@ -26,22 +26,52 @@ class _CustomDropDownWidgetState extends State<CustomDropDownWidget> {
           child: Column(
             children: [
               SizedBox(height: 50,),
-              Container(
-                height: 50,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade300
+              InkWell(
+                onTap: (){
+                  isOpen = !isOpen;
+                  setState(() {
+
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300
+                  ),
+                  child : Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(selectOptions),
+                      Icon(isOpen ? Icons.keyboard_arrow_up : Icons.keyboard_arrow_down)
+                    ],
+                  ),
+                  ),
                 ),
-                child : Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(selectOptions),
-                    Icon(Icons.keyboard_arrow_down)
-                  ],
-                ),
-                ),
+              ),
+
+              ListView(
+                primary: true,
+                shrinkWrap: true,
+                children : politicsList.map((e) => Container(
+                  decoration: BoxDecoration(
+                      color: selectOptions == e ? Colors.red : Colors.grey.shade300
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: InkWell(
+                        onTap: (){
+                          selectOptions = e;
+                          isOpen = false;
+                          setState(() {
+
+                          });
+                        },
+                        child: Text(e)),
+                  ),
+                )).toList(),
               ),
             ],
           ),
