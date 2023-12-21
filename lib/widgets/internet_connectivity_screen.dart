96c5e7879a4cf3a9d5d633d0bfa9120d9387d222
Atikuzzaman Sabuj <113611885+atik-sabuj@users.noreply.github.com>
2023-12-21 12:05:40 +0600

    Update internet_connectivity_screen.dart

--- lib/widgets/internet_connectivity_screen.dart
+++ lib/widgets/internet_connectivity_screen.dart
@@ -1,15 +1,35 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
-class InternetConnectivityScreen extends StatefulWidget {
+class InternetConnectivityScreen extends StatelessWidget {
   const InternetConnectivityScreen({super.key});
 
   @override
-  State<InternetConnectivityScreen> createState() => _InternetConnectivityScreenState();
+  Widget build(BuildContext context) {
+
+    Connectivity connectivity = Connectivity();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Internet Connectivity Screen'),
+        centerTitle: true,
+      ),
+
+      body: StreamBuilder<ConnectivityResult>(
+        stream: connectivity.onConnectivityChanged,
+        builder: (_, snapshot){
+          return InternetConnectionWidget();
+        },
+      ),
+    );
+  }
 }
 
-class _InternetConnectivityScreenState extends State<InternetConnectivityScreen> {
+class InternetConnectionWidget extends StatelessWidget {
+  const InternetConnectionWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
