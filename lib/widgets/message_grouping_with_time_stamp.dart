240343fd396a19b8705173f0cc545da26620df84
Atikuzzaman Sabuj <113611885+atik-sabuj@users.noreply.github.com>
2023-12-23 12:08:24 +0600

    Update message_grouping_with_time_stamp.dart

--- lib/widgets/message_grouping_with_time_stamp.dart
+++ lib/widgets/message_grouping_with_time_stamp.dart
@@ -298,3 +298,190 @@ class MessageBubble extends CustomPainter {
 
   final double _radius = 10.0;
 
+  @override
+  void paint(Canvas canvas, Size size) {
+    var h = size.height;
+    var w = size.width;
+    if (alignment == Alignment.topRight) {
+      if (tail) {
+        var path = Path();
+
+        /// starting point
+        path.moveTo(_radius * 2, 0);
+
+        /// top-left corner
+        path.quadraticBezierTo(0, 0, 0, _radius * 1.5);
+
+        /// left line
+        path.lineTo(0, h - _radius * 1.5);
+
+        /// bottom-left corner
+        path.quadraticBezierTo(0, h, _radius * 2, h);
+
+        /// bottom line
+        path.lineTo(w - _radius * 3, h);
+
+        /// bottom-right bubble curve
+        path.quadraticBezierTo(
+            w - _radius * 1.5, h, w - _radius * 1.5, h - _radius * 0.6);
+
+        /// bottom-right tail curve 1
+        path.quadraticBezierTo(w - _radius * 1, h, w, h);
+
+        /// bottom-right tail curve 2
+        path.quadraticBezierTo(
+            w - _radius * 0.8, h, w - _radius, h - _radius * 1.5);
+
+        /// right line
+        path.lineTo(w - _radius, _radius * 1.5);
+
+        /// top-right curve
+        path.quadraticBezierTo(w - _radius, 0, w - _radius * 3, 0);
+
+        canvas.clipPath(path);
+        canvas.drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill);
+      } else {
+        var path = Path();
+
+        /// starting point
+        path.moveTo(_radius * 2, 0);
+
+        /// top-left corner
+        path.quadraticBezierTo(0, 0, 0, _radius * 1.5);
+
+        /// left line
+        path.lineTo(0, h - _radius * 1.5);
+
+        /// bottom-left corner
+        path.quadraticBezierTo(0, h, _radius * 2, h);
+
+        /// bottom line
+        path.lineTo(w - _radius * 3, h);
+
+        /// bottom-right curve
+        path.quadraticBezierTo(w - _radius, h, w - _radius, h - _radius * 1.5);
+
+        /// right line
+        path.lineTo(w - _radius, _radius * 1.5);
+
+        /// top-right curve
+        path.quadraticBezierTo(w - _radius, 0, w - _radius * 3, 0);
+
+        canvas.clipPath(path);
+        canvas.drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill);
+      }
+    } else {
+      if (tail) {
+        var path = Path();
+
+        /// starting point
+        path.moveTo(_radius * 3, 0);
+
+        /// top-left corner
+        path.quadraticBezierTo(_radius, 0, _radius, _radius * 1.5);
+
+        /// left line
+        path.lineTo(_radius, h - _radius * 1.5);
+        // bottom-right tail curve 1
+        path.quadraticBezierTo(_radius * .8, h, 0, h);
+
+        /// bottom-right tail curve 2
+        path.quadraticBezierTo(
+            _radius * 1, h, _radius * 1.5, h - _radius * 0.6);
+
+        /// bottom-left bubble curve
+        path.quadraticBezierTo(_radius * 1.5, h, _radius * 3, h);
+
+        /// bottom line
+        path.lineTo(w - _radius * 2, h);
+
+        /// bottom-right curve
+        path.quadraticBezierTo(w, h, w, h - _radius * 1.5);
+
+        /// right line
+        path.lineTo(w, _radius * 1.5);
+
+        /// top-right curve
+        path.quadraticBezierTo(w, 0, w - _radius * 2, 0);
+        canvas.clipPath(path);
+        canvas.drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill);
+      } else {
+        var path = Path();
+
+        /// starting point
+        path.moveTo(_radius * 3, 0);
+
+        /// top-left corner
+        path.quadraticBezierTo(_radius, 0, _radius, _radius * 1.5);
+
+        /// left line
+        path.lineTo(_radius, h - _radius * 1.5);
+
+        /// bottom-left curve
+        path.quadraticBezierTo(_radius, h, _radius * 3, h);
+
+        /// bottom line
+        path.lineTo(w - _radius * 2, h);
+
+        /// bottom-right curve
+        path.quadraticBezierTo(w, h, w, h - _radius * 1.5);
+
+        /// right line
+        path.lineTo(w, _radius * 1.5);
+
+        /// top-right curve
+        path.quadraticBezierTo(w, 0, w - _radius * 2, 0);
+        canvas.clipPath(path);
+        canvas.drawRRect(
+            RRect.fromLTRBR(0, 0, w, h, Radius.zero),
+            Paint()
+              ..color = color
+              ..style = PaintingStyle.fill);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+
+
+
+/// {@template custom_rect_tween}
+/// Linear RectTween with a [Curves.easeOut] curve.
+///
+/// Less dramatic that the regular [RectTween] used in [Hero] animations.
+/// {@endtemplate}
+class CustomRectTween extends RectTween {
+  /// {@macro custom_rect_tween}
+  CustomRectTween({
+    required Rect begin,
+    required Rect end,
+  }) : super(begin: begin, end: end);
+
+  @override
+  Rect lerp(double t) {
+    final elasticCurveValue = Curves.easeOut.transform(t);
+    return Rect.fromLTRB(
+      lerpDouble(begin!.left, end!.left, elasticCurveValue)!,
+      lerpDouble(begin!.top, end!.top, elasticCurveValue)!,
+      lerpDouble(begin!.right, end!.right, elasticCurveValue)!,
+      lerpDouble(begin!.bottom, end!.bottom, elasticCurveValue)!,
+    );
+  }
+}
