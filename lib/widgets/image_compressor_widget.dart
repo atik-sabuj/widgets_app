53a25c55c7e3c139080a71749955013687c81c96
atik-sabuj <sabu235@gmail.com>
2023-12-24 17:06:12 +0600

    Image Compressor Widget

---
+++ lib/widgets/image_compressor_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImageCompressorWidget extends StatefulWidget {
+  const ImageCompressorWidget({super.key});
+
+  @override
+  State<ImageCompressorWidget> createState() => _ImageCompressorWidgetState();
+}
+
+class _ImageCompressorWidgetState extends State<ImageCompressorWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
