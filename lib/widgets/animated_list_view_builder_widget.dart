90b115edc51ab1c1bf31878383f7e57e42c0ab61
atik-sabuj <sabu235@gmail.com>
2023-12-24 15:03:05 +0600

    Animated List View Builder Widget

---
+++ lib/widgets/animated_list_view_builder_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatedListViewBuilderWidget extends StatefulWidget {
+  const AnimatedListViewBuilderWidget({super.key});
+
+  @override
+  State<AnimatedListViewBuilderWidget> createState() => _AnimatedListViewBuilderWidgetState();
+}
+
+class _AnimatedListViewBuilderWidgetState extends State<AnimatedListViewBuilderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
