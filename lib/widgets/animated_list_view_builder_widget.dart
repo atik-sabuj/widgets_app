572e97cabca7966fc90fc5a91b7d217a2100ee7b
Atikuzzaman Sabuj <113611885+atik-sabuj@users.noreply.github.com>
2023-12-24 15:54:20 +0600

    Update animated_list_view_builder_widget.dart

--- lib/widgets/animated_list_view_builder_widget.dart
+++ lib/widgets/animated_list_view_builder_widget.dart
@@ -8,6 +8,12 @@ class AnimatedListViewBuilderWidget extends StatefulWidget {
 }
 
 class _AnimatedListViewBuilderWidgetState extends State<AnimatedListViewBuilderWidget> {
+
+  final GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
+
+  List<int> item = [];
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +21,35 @@ class _AnimatedListViewBuilderWidgetState extends State<AnimatedListViewBuilderW
         title: Text('Animated List View Builder Widget'),
         centerTitle: true,
       ),
-      body: Column(),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          listKey.currentState!.insertItem(0, duration: Duration(milliseconds: 400));
+          item = []..add(count++)
+          ..addAll(item);
+        },
+      ),
+
+      body: SafeArea(
+        child: AnimatedList(
+          key: listKey,
+          initialItemCount: item.length,
+          itemBuilder: (context, index, animation) {
+            return SizeTransition(
+                sizeFactor: animation,
+              axis: Axis.vertical,
+              child: SizedBox(
+                height: 100,
+                width: double.infinity,
+                child: Card(
+                  color: Colors.primaries[index % Colors.primaries.length],
+                  child: Text('item '+item[index].toString(), style: Theme.of(context).textTheme.headlineSmall,),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
