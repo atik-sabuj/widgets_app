a3cac851fb28c804246affe656b61bdec7c45ea0
atik-sabuj <sabuj235@gmail.com>
2023-12-27 17:54:31 +0600

    Image Zoom Viewer Widget #2

--- lib/widgets/image/ineractive_viewer_widget.dart
+++ lib/widgets/image/ineractive_viewer_widget.dart
@@ -13,9 +13,11 @@ class InteractiveViewerWidget extends StatelessWidget {
         child: InteractiveViewer(
           minScale: 1,
           maxScale: 10,
-          child: Image.asset(
-              'images/aboy.jpeg'
-          ),
+          child:
+          //Image.asset('images/aboy.jpeg'),
+
+          Image.network('https://images.prothomalo.com/prothomalo-english%2Fimport%2Fmedia%2F2019%2F07%2F11%2F74bfcc523b223bd0a7c0710f15c7ce6f-Sundarbans.jpg?rect=0%2C72%2C1024%2C576&auto=format%2Ccompress&fmt=webp&format=webp&w=640&dpr=2.0'),
+
         ),
       ),
     );
