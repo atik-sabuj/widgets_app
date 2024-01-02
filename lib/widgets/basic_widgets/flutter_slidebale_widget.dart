d339f912c938a3533b18b0e8a09cc61c8c12a91a
atik-sabuj <sabuj235@gmail.com>
2024-01-02 09:48:57 +0600

    Image Slider Widget #4

--- lib/widgets/basic_widgets/flutter_slidebale_widget.dart
+++ lib/widgets/basic_widgets/flutter_slidebale_widget.dart
@@ -8,6 +8,13 @@ class ImageSliderWidget extends StatefulWidget {
   State<ImageSliderWidget> createState() => _ImageSliderWidgetState();
 }
 
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+];
+
+
 class _ImageSliderWidgetState extends State<ImageSliderWidget> {
   @override
   Widget build(BuildContext context) {
@@ -33,9 +40,8 @@ class _ImageSliderWidgetState extends State<ImageSliderWidget> {
               ),
 
               items: [
-
+                Text('Sabuj Atik')
             ],
-
 
             ),
           ),
@@ -44,4 +50,10 @@ class _ImageSliderWidgetState extends State<ImageSliderWidget> {
       ),
     );
   }
+
+
+
+
+
+
 }
