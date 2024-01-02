077c4cb65b94679a8653f5ff63203d01a5a9e1f9
atik-sabuj <sabuj235@gmail.com>
2024-01-02 10:06:09 +0600

    Image Slider Widget #6

--- lib/widgets/basic_widgets/flutter_slidebale_widget.dart
+++ lib/widgets/basic_widgets/flutter_slidebale_widget.dart
@@ -65,13 +65,37 @@ final List<Widget> imageSliders = imgList
             bottom: 0.0,
             left: 0.0,
             right: 0.0,
-          ),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(200, 0, 0, 0),
+                    Color.fromARGB(0, 0, 0, 0),
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+
+              padding: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 20.0),
+              child: Text(
+                'No. ${imgList.indexOf(item)} image',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              ),
+            ),
         ],
-      ),
+      )
     ),
   ),
-));
 
+)).toList();
 
 
 }
