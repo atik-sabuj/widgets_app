ec818202572a6c4bffe61cd5fc6e3776ae46df98
atik-sabuj <sabuj235@gmail.com>
2024-01-02 11:04:07 +0600

    Number Picker Widget #5

--- lib/widgets/basic_widgets/number_picker_widget.dart
+++ lib/widgets/basic_widgets/number_picker_widget.dart
@@ -2,7 +2,7 @@ import 'package:awesome_number_picker/awesome_number_picker.dart';
 import 'package:flutter/material.dart';
 
 class NumberPickerWidget extends StatefulWidget {
-  const NumberPickerWidget({super.key});
+  const NumberPickerWidget({Key? key}) : super(key: key);
 
   @override
   State<NumberPickerWidget> createState() => _NumberPickerWidgetState();
@@ -26,7 +26,7 @@ class _NumberPickerWidgetState extends State<NumberPickerWidget> {
         children: [
           InkWell(
             onTap: (){
-              //_showMyDialg();
+              _showMyDialog();
             },
 
             child: Row(
@@ -38,6 +38,7 @@ class _NumberPickerWidgetState extends State<NumberPickerWidget> {
               ],
             ),
           ),
+
 
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -74,15 +75,61 @@ class _NumberPickerWidgetState extends State<NumberPickerWidget> {
                   ),
                 ],
               ),
-
-
+              onChanged: (i) => setState(() {
+                decimalValue = i;
+              }),
             ),
           ),
-
         ],
       ),
     );
   }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,   //user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('AlertDialog Title'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                SizedBox(
+                  height: 80,
+                  child: IntegerNumberPicker(
+                    initialValue: integerValue,
+                    axis: Axis.vertical,
+                    pickedItemDecoration: BoxDecoration(
+                      color: Colors.transparent
+                  ),
+              otherItemsDecoration: BoxDecoration(
+                color: Colors.transparent
+              ),
+              minValue: 0,
+              maxValue: 10,
+              onChanged: (i) => setState(() {
+                integerValue = i;
+              }),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+        actions: [
+          TextButton(
+          child: Text('Approve'),
+          onPressed: (){
+            Navigator.of(context).pop();
+        },
+        ),
+          ],
+        );
+      }
+    );
+  }
+
 }
 
 
