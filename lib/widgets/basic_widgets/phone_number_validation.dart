a0010a38cc58e35e394e1f1dd8f5ee06befd1328
atik-sabuj <sabuj235@gmail.com>
2024-01-03 09:44:11 +0600

    Phone Number Validation #2

--- lib/widgets/basic_widgets/phone_number_validation.dart
+++ lib/widgets/basic_widgets/phone_number_validation.dart
@@ -8,8 +8,38 @@ class PhoneNumberValidation extends StatefulWidget {
 }
 
 class _PhoneNumberValidationState extends State<PhoneNumberValidation> {
+  bool outlineBorder = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      //drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text('Phone Form Field'),
+        centerTitle: true,
+      ),
+      
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 600),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    SwitchListTile(
+                        value: outlineBorder,
+                        onChanged: (v) => setState(() => outlineBorder = v ),
+                      title: Text('Outlined border'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
