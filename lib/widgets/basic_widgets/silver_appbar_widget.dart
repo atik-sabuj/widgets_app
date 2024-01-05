217644c29d2ad87ed29a76b58e0ebbd35e8f940c
atik-sabuj <sabuj235@gmail.com>
2024-01-05 19:23:01 +0600

    Silver AppBar Widget #3

--- lib/widgets/basic_widgets/silver_appbar_widget.dart
+++ lib/widgets/basic_widgets/silver_appbar_widget.dart
@@ -11,9 +11,24 @@ class SilverAppBarWidget extends StatelessWidget {
         centerTitle: true,
       ),
 
-      body: Center(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              snap: false,
+              pinned: false,
+              floating: false,
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text("$title",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ) //TextStyle
+                  ), //Text
+                  background: Image.network(
+                    "https://i.ibb.co/QpWGK5j/Geeksfor-Geeks.png",
+                    fit: BoxFit.cover,
+                  ) //Images.network
+              ),
 
-      ),
-    );
-  }
-}
+
