363eb72c8f91c83bf91c3ea6e644d0d22046edb8
atik-sabuj <sabuj235@gmail.com>
2024-01-06 13:11:44 +0600

    Alarm Clock Widget #3

--- lib/widgets/fromsite_widgets/alarm_clock_widget.dart
+++ lib/widgets/fromsite_widgets/alarm_clock_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_alarm_clock/flutter_alarm_clock.dart';
 
 class AlarmClockWidget extends StatefulWidget {
   const AlarmClockWidget({super.key});
@@ -8,6 +9,14 @@ class AlarmClockWidget extends StatefulWidget {
 }
 
 class _AlarmClockWidgetState extends State<AlarmClockWidget> {
+
+
+  // creating text editing controller to take hour
+  // and minute as input
+  TextEditingController hourController = TextEditingController();
+  TextEditingController minuteController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +26,22 @@ class _AlarmClockWidgetState extends State<AlarmClockWidget> {
       ),
 
       body: Center(
-
-      ),
-    );
-  }
-}
+          child: Column(children: <Widget>[
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(11)),
+                  child: Center(
+                    child: TextField(
+                      controller: hourController,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ),
