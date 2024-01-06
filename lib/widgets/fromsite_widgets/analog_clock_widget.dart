bc929461c3d37f720498601febe5bc1e88554610
atik-sabuj <sabuj235@gmail.com>
2024-01-06 12:53:23 +0600

    Analog Clock Widget #3

--- lib/widgets/fromsite_widgets/analog_clock_widget.dart
+++ lib/widgets/fromsite_widgets/analog_clock_widget.dart
@@ -23,6 +23,19 @@ class _AnalogClockWidgetState extends State<AnalogClockWidget> {
               border: Border.all(width: 3.0, color: Colors.black),
               color: Colors.black,
               shape: BoxShape.circle),
+          // decoration
+          width: 200.0,
+          isLive: true,
+          hourHandColor: Colors.white,
+          minuteHandColor: Colors.white,
+          showSecondHand: true,
+          numberColor: Colors.white,
+          showNumbers: true,
+          textScaleFactor: 1.5,
+          showTicks: true,
+          showDigitalClock: true,
+          digitalClockColor: Colors.white,
+          datetime: DateTime(2020, 8, 4, 9, 11, 0),
         ), // analog clock
       ), // center
     );
