8af4e2826a2c324fbad6deaf68645becf05aa8ca
atik-sabuj <sabuj235@gmail.com>
2024-01-06 12:51:51 +0600

    Analog Clock Widget #2

--- lib/widgets/fromsite_widgets/analog_clock_widget.dart
+++ lib/widgets/fromsite_widgets/analog_clock_widget.dart
@@ -1,10 +1,30 @@
+import 'package:analog_clock/analog_clock.dart';
 import 'package:flutter/material.dart';
 
-class AnalogClockWidget extends StatelessWidget {
+class AnalogClockWidget extends StatefulWidget {
   const AnalogClockWidget({super.key});
 
   @override
+  State<AnalogClockWidget> createState() => _AnalogClockWidgetState();
+}
+
+class _AnalogClockWidgetState extends State<AnalogClockWidget> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[900],
+        title: const Text('Analog Clock Widget'),
+      ), // app bar
+      backgroundColor: Colors.green,
+      body: Center(
+        child: AnalogClock(
+          decoration: BoxDecoration(
+              border: Border.all(width: 3.0, color: Colors.black),
+              color: Colors.black,
+              shape: BoxShape.circle),
+        ), // analog clock
+      ), // center
+    );
   }
 }
