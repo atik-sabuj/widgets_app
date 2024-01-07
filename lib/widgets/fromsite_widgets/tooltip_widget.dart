71307be3faf8d124ae4e945b2d88898b1acf86c0
atik-sabuj <sabuj235@gmail.com>
2024-01-07 18:10:53 +0600

    Tooltip Widget #2

--- lib/widgets/fromsite_widgets/tooltip_widget.dart
+++ lib/widgets/fromsite_widgets/tooltip_widget.dart
@@ -5,6 +5,15 @@ class TooltipWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tooltip Widget'),
+        centerTitle: true,
+      ),
+
+      body: Center(
+
+      ),
+    );
   }
 }
