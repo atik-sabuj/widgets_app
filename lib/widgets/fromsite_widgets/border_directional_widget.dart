4720e54a9482d5a12fced87e27a5e59361b70076
atik-sabuj <sabuj235@gmail.com>
2024-01-07 18:01:19 +0600

    Border Directional Widget #2

--- lib/widgets/fromsite_widgets/border_directional_widget.dart
+++ lib/widgets/fromsite_widgets/border_directional_widget.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
-class BorderDirectionalWidget extends StatelessWidget {
+class BorderDirectionalWidget extends StatefulWidget {
   const BorderDirectionalWidget({super.key});
 
   @override
+  State<BorderDirectionalWidget> createState() => _BorderDirectionalWidgetState();
+}
+
+class _BorderDirectionalWidgetState extends State<BorderDirectionalWidget> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Border Directional Widget'),
+      ),
+
+      body: Center(
+
+      ),
+    );
   }
 }
