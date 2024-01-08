c0a927dd7614e5dff1c9c31c35c8b099e1ca523e
atik-sabuj <sabuj235@gmail.com>
2024-01-08 09:35:41 +0600

    Inkwell Widget #2

--- lib/widgets/fromsite_widgets/inkwell_widget.dart
+++ lib/widgets/fromsite_widgets/inkwell_widget.dart
@@ -15,50 +15,19 @@ class _InkwellWidgetState extends State<InkwellWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Raised Button Widget'),
+        title: Text('Inkwell Widget'),
+        centerTitle: true,
         backgroundColor: Colors.green,
-      ),
-
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // RaisedButton has been deprecated
-            // We can use Elevated button achieve the same results
-            RaisedButton(
-              //     disabledColor: Colors.red,
-              // disabledTextColor: Colors.black,
-              padding: const EdgeInsets.all(20),
-              textColor: Colors.white,
-              color: Colors.green,
-              onPressed: () {
-                setState(() {
-                  istapped = 'Button tapped';
-                });
-              },
-              child: const Text('Enabled Button'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            // ElevatedButton
-            InkWell(
-                onPressed: () {
-                  setState(() {
-                    istapped = 'Button tapped';
-                  });
-                },
-                child: const Text('Enabled Button')),
-            const SizedBox(height: 20),
-            Text(
-              istapped,
-              // ignore: deprecated_member_use
-              textScaleFactor: 2,
-            )
-          ],
-        ),
+        actions: <Widget>[
+          Text(
+            'GFG',
+            // ignore: deprecated_member_use
+            textScaleFactor: 3,
+          )
+        ],
       ),
       backgroundColor: Colors.lightBlue[50],
+
     );
   }
 }
