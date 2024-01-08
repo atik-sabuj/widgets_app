ad6ce26f2ff7fe9c6f725625691a99662292fd82
atik-sabuj <sabuj235@gmail.com>
2024-01-08 09:20:08 +0600

    Raised Button Widget #3

--- lib/widgets/fromsite_widgets/raised_button_widget.dart
+++ lib/widgets/fromsite_widgets/raised_button_widget.dart
@@ -8,16 +8,40 @@ class RaisedButtonWidget extends StatefulWidget {
 }
 
 class _RaisedButtonWidgetState extends State<RaisedButtonWidget> {
+
+  String istapped = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Raised Button Widget'),
-        centerTitle: true,
+        backgroundColor: Colors.green,
       ),
 
       body: Center(
-
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+        // RaisedButton has been deprecated
+        // We can use Elevated button achieve the same results
+        RaisedButton(
+        //     disabledColor: Colors.red,
+        // disabledTextColor: Colors.black,
+        padding: const EdgeInsets.all(20),
+        textColor: Colors.white,
+        color: Colors.green,
+        onPressed: () {
+          setState(() {
+            istapped = 'Button tapped';
+          });
+        },
+        child: const Text('Enabled Button'),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+        ),
       ),
     );
   }
