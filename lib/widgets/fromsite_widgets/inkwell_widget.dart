899a73fde9f4b04dd6f91342fd9a746816fc12ee
atik-sabuj <sabuj235@gmail.com>
2024-01-08 09:35:57 +0600

    Inkwell Widget #3

--- lib/widgets/fromsite_widgets/inkwell_widget.dart
+++ lib/widgets/fromsite_widgets/inkwell_widget.dart
@@ -27,7 +27,42 @@ class _InkwellWidgetState extends State<InkwellWidget> {
         ],
       ),
       backgroundColor: Colors.lightBlue[50],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            InkWell(
+              onTap: () {
+                setState(() {
+                  inkwell='Inkwell Tapped';
+                });
+              },
+              onLongPress: () {
+                setState(() {
+                  inkwell='InkWell Long Pressed';
+                });
+              },
+              child: Container(
+                  color: Colors.green,
+                  width: 120,
+                  height: 70,
+                  child: Center(
+                      child: Text(
+                        'Inkwell',
+                        // ignore: deprecated_member_use
+                        textScaleFactor: 2,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ))),
+            ),
 
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              // ignore: deprecated_member_use
+              child: Text(inkwell,textScaleFactor: 2,),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
