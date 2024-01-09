8454a14af85ef1ae38b9b2b7625fcaa93f2bf34a
atik-sabuj <sabuj235@gmail.com>
2024-01-09 09:36:27 +0600

    ClipOval Widget#2

--- lib/widgets/fromsite_widgets/clipoval_widget.dart
+++ lib/widgets/fromsite_widgets/clipoval_widget.dart
@@ -5,6 +5,15 @@ class ClipOvalWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Clip Oval Widget'),
+        backgroundColor: Colors.green,
+      ),
+
+      body: Center(
+
+      ),
+    );
   }
 }
