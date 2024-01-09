17662b1d813bd552e1b28c4bdb2486c32280d70b
atik-sabuj <sabuj235@gmail.com>
2024-01-09 09:22:18 +0600

    Octo Image Widget #4

--- lib/widgets/fromsite_widgets/octo_image_widget.dart
+++ lib/widgets/fromsite_widgets/octo_image_widget.dart
@@ -24,3 +24,23 @@ class OctoImageWidget extends StatelessWidget {
     );
   }
 
+  // circularProgressIndicator
+  Widget _customImage() {
+    return SizedBox(
+      height: 200,
+      child: OctoImage(
+        image: NetworkImage('https://www.irishtimes.com/polopoly_fs/1.4109315.1575891315!/image/image.jpg_gen/derivatives/box_620_330/image.jpg'),
+        progressIndicatorBuilder: (context, progress) {
+          double value;
+          if (progress != null && progress.expectedTotalBytes != null) {
+            value =
+                progress.cumulativeBytesLoaded / progress.expectedTotalBytes;
+          }
+          return CircularProgressIndicator(value: value);
+        },
+        errorBuilder: (context, error, stacktrace) => Icon(Icons.error),
+      ),
+    );
+  }
+
+
