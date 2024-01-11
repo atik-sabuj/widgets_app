882c264e9ae84399546dc7a2f1351bce8cd3cbca
atik-sabuj <sabuj235@gmail.com>
2024-01-11 11:00:39 +0600

    Clip Oval Widget #4

--- lib/widgets/fromsite_widgets/clipoval_widget.dart
+++ lib/widgets/fromsite_widgets/clipoval_widget.dart
@@ -19,33 +19,6 @@ class ClipOvalWidget extends StatelessWidget {
           clipper: MyClip(),
         ),
 
-        //2nd 
-        ClipOval(
-          child: Image.network(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRFU7U2h0umyF0P6E_yhTX45sGgPEQAbGaJ4g&usqp=CAU',
-              fit: BoxFit.fill),
-          clipper: MyClip(),
-        ),
-
-        ClipOval(
-          child: Image.network(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRFU7U2h0umyF0P6E_yhTX45sGgPEQAbGaJ4g&usqp=CAU',
-              fit: BoxFit.fill),
-          clipper: MyClip(),
-        ),
-
-        ClipOval(
-          child: Image.network(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRFU7U2h0umyF0P6E_yhTX45sGgPEQAbGaJ4g&usqp=CAU',
-              fit: BoxFit.fill),
-          clipper: MyClip(),
-        ),
-        ClipOval(
-          child: Image.network(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRFU7U2h0umyF0P6E_yhTX45sGgPEQAbGaJ4g&usqp=CAU',
-              fit: BoxFit.fill),
-          clipper: MyClip(),
-        ),
       ),
       backgroundColor: Colors.lightBlue[50],
     );
