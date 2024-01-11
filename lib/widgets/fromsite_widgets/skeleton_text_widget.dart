a205cb3e5e9d51e73343f1bc49a1e1d437bbe146
atik-sabuj <sabuj235@gmail.com>
2024-01-11 10:58:52 +0600

    Skeleton Text Widget #3

--- lib/widgets/fromsite_widgets/skeleton_text_widget.dart
+++ lib/widgets/fromsite_widgets/skeleton_text_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skeleton_text/skeleton_text.dart';
 
 class SkeletonTextWidget extends StatelessWidget {
   const SkeletonTextWidget({super.key});
@@ -11,9 +12,33 @@ class SkeletonTextWidget extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
 
-      body: Center(
+      // list of items in body
+      body: ListView.builder(
+          scrollDirection: Axis.vertical,
+          physics: BouncingScrollPhysics(),
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    color: Colors.white70),
+                child: Container(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
 
-      ),
+
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
     );
   }
 }
