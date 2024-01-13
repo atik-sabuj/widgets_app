36fa4d918393c4db618063f6c8882092b9d60283
atik-sabuj <sabuj235@gmail.com>
2024-01-13 09:23:07 +0600

    Photo Hero Widget #2

--- lib/widgets/fromsite_widgets/photo_hero_widget.dart
+++ lib/widgets/fromsite_widgets/photo_hero_widget.dart
@@ -5,6 +5,13 @@ class PhotoHeroWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PhotoHero Widget'),
+        centerTitle: true,
+      ),
+
+      body: Center(),
+    );
   }
 }
