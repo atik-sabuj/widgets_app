ce731b9cb21626e27d6c4664b748bdb54841291a
Atikuzzaman Sabuj <113611885+atik-sabuj@users.noreply.github.com>
2024-01-20 11:20:30 +0600

    Update photo_hero_widget.dart

--- lib/widgets/fromsite_widgets/photo_hero_widget.dart
+++ lib/widgets/fromsite_widgets/photo_hero_widget.dart
@@ -226,6 +226,33 @@ enter(
                   backgroundColor: Colors.green,
                 ),
 
+
+
+                                         body: Container(
+                  // background of 2nd route
+                  color: Colors.purple,
+                  padding: const EdgeInsets.all(16.0),
+                  alignment: Alignment.topLeft,
+                  child: PhotoHeroWidget(
+                    photo: 'images/aboy.jpeg',
+                    width: 100.0,
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              );
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(home: HeroAnimation()));
+
 }
                  
       
