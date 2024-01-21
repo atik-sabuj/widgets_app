082f77e2b76e4bd498b1582cc607a154c06c99bf
atik-sabuj <sabuj235@gmail.com>
2024-01-21 15:50:28 +0600

    Screen Orientation #4

--- lib/widgets/fromsite_widgets/ui_orientation.dart
+++ lib/widgets/fromsite_widgets/ui_orientation.dart
@@ -18,7 +18,15 @@ class ScreenOrientation extends StatelessWidget {
 
             //grid with 3 and 4 columns for portrait and landscape mode respectively
             crossAxisCount: orientation == Orientation.portrait ? 3 : 4,
-
+            children: List.generate(100, (index) {
+              return Center(
+                child: Text(
+                  'A $index',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              );
+            }),
+          );
             // random item generator
 
         },
