b318259e9c1c4ac6311decd9ea8cc8b8f75ca791
atik-sabuj <sabuj235@gmail.com>
2024-01-22 10:14:35 +0600

    Animated Transition Page One #4

--- lib/widgets/fromsite_widgets/animation_route_transition.dart
+++ lib/widgets/fromsite_widgets/animation_route_transition.dart
@@ -14,7 +14,7 @@ class AnimatedTransitionPageOne extends StatelessWidget {
       body: Center(
           child: ElevatedButton(
             onPressed: () {
-             // Navigator.of(context).push(_createRoute());
+              Navigator.of(context).push(_createRoute());
             },
             child: const Text('Go to Page 2'),
           )
@@ -32,4 +32,23 @@ class AnimatedTransitionPageOne extends StatelessWidget {
       ),
     );
   }
+}
+
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = const Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
