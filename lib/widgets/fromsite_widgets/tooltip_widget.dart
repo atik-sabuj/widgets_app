69f21a66965a094609d6e4eb29de01978c876387
Atikuzzaman Sabuj <113611885+atik-sabuj@users.noreply.github.com>
2024-02-05 12:01:11 +0600

    Update tooltip_widget.dart

--- lib/widgets/fromsite_widgets/tooltip_widget.dart
+++ lib/widgets/fromsite_widgets/tooltip_widget.dart
@@ -80,3 +80,34 @@ body: Center(
 
   } //MaterialApp
   }
+
+
+body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Tooltip(
+                    message: 'Text',
+                    child: Text(
+                      'Flutter is an open-source UI software development kit created by Google. ',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 25,
+                      ), //TextStyle
+                    ), //Text
+                  ), //Tooltip
+                ), //Padding
+                color: Colors.green[50],
+                width: 300,
+                height: 175,
+              ), //Container
+            ], //<Widget>[]
+          ) //Row
+      ), //Center
+    );
+
+  } //MaterialApp
+  }
